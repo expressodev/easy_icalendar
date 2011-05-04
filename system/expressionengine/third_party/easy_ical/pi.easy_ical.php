cf35b12d2d92a59c16c209113a25b620883f9070
Adrian Macneil <adrian@crescendo.net.nz>
2011-05-05 11:44:39 +1200

    Removing unnecessary whitespace

--- system/expressionengine/third_party/easy_ical/pi.easy_ical.php
+++ system/expressionengine/third_party/easy_ical/pi.easy_ical.php
@@ -2,10 +2,10 @@
 
 /*
  * Easy iCalendar plugin by Crescendo (support@crescendo.net.nz)
- * 
+ *
  * Copyright (c) 2010 Crescendo Multimedia Ltd
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
@@ -16,7 +16,7 @@
  *    documentation and/or other materials provided with the distribution.
  * 3. The name of the author may not be used to endorse or promote products
  *    derived from this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
@@ -44,30 +44,30 @@ class Easy_ical
 	{
 		$this->EE =& get_instance();
 	}
-	
+
 	function calendar()
 	{
 		$out = "BEGIN:VCALENDAR\r\nVERSION:2.0\r\n";
-		
+
 		if ($this->EE->TMPL->fetch_param('timezone') !== FALSE)
 		{
 			$out .= "X-WR-TIMEZONE:".$this->escape($this->EE->TMPL->fetch_param('timezone'))."\r\n";
 		}
-		
+
 		if ($this->EE->TMPL->fetch_param('calname') !== FALSE)
 		{
 			$out .= "X-WR-CALNAME:".$this->escape($this->EE->TMPL->fetch_param('calname'))."\r\n";
 		}
-		
+
 		$out .= "PRODID:-//EE Easy iCalendar plugin//NONSGML v1.1//EN\r\n";
-		
+
 		// EE has probably put heaps of useless whitespace between each entry
 		$tagdata = trim($this->EE->TMPL->tagdata);
 		$tagdata = preg_replace('/END\:VEVENT\s*BEGIN\:VEVENT/', "END:VEVENT\r\nBEGIN:VEVENT", $tagdata);
 		if (!empty($tagdata)) $out .= $tagdata."\r\n";
-		
+
 		$out .= "END:VCALENDAR";
-		
+
 		// print output directly with the correct content-type
 		$content_type = $this->EE->TMPL->fetch_param('content_type');
 		if (empty($content_type)){
@@ -78,34 +78,34 @@ class Easy_ical
 			$content_type = 'text/html; charset=UTF-8';
 			$out ="<pre>".$out;
 		}
-		
+
 		header('Content-Type: '.$content_type);
 		exit($out);
 	}
-	
+
 	function event()
 	{
 		$out = "BEGIN:VEVENT\r\n".
 			"UID:".$this->escape($this->EE->TMPL->fetch_param('uid'))."\r\n";
-		
+
 		if ($this->EE->TMPL->fetch_param('location') !== FALSE)
 		{
 			$out .= "LOCATION:".$this->escape($this->EE->TMPL->fetch_param('location'))."\r\n";
 		}
-		
+
 		$out .= "DTSTAMP:".$this->ical_time($this->EE->TMPL->fetch_param('start_time'))."\r\n";
 		$out .= "DTSTART:".$this->ical_time($this->EE->TMPL->fetch_param('start_time'))."\r\n";
-		
+
 		if ($this->EE->TMPL->fetch_param('end_time') !== FALSE)
 		{
 			$out .= "DTEND:".$this->ical_time($this->EE->TMPL->fetch_param('end_time'))."\r\n";
 		}
-		
+
 		if ($this->EE->TMPL->fetch_param('summary') !== FALSE)
 		{
 			$out .= "SUMMARY:".$this->escape($this->EE->TMPL->fetch_param('summary'))."\r\n";
 		}
-		
+
 		if ($this->EE->TMPL->fetch_param('update') !== FALSE)
 		{
 			if($this->EE->TMPL->fetch_param('update') !=='')
@@ -117,18 +117,18 @@ class Easy_ical
 		{
 			$out .= "URL:".$this->escape($this->EE->TMPL->fetch_param('url'))."\r\n";
 		}
-		
+
 		$description = trim($this->EE->TMPL->tagdata);
 		if (!empty($description))
 		{
 			$out .= "DESCRIPTION:".$this->escape($description)."\r\n";
 		}
-		
+
 		$out .= "END:VEVENT"."\r\n";
-		
+
 		return $out;
 	}
-	
+
 	function escape($str)
 	{
 		// strip any html tags
@@ -136,28 +136,28 @@ class Easy_ical
 		$str = preg_replace('/\<br\s*\/?\>/i', "\n", $str);
 		$str = strip_tags($str);
 		$str = trim(html_entity_decode($str, ENT_QUOTES, 'UTF-8'));
-		
+
 		// no more than two newlines please
 		$str = preg_replace("/(\r?\n){3,}/", "\n\n", $str);
-		
+
 		// lines can't be more than 75 chars, use 60 to be safe
 		$lines = str_split($str, 60);
-		
+
 		foreach ($lines as $key => $line)
 		{
 			// escape special icalendar chars and convert newlines to '\n'
 			$lines[$key] = str_replace(array('\\', ',', ';'), array('\\\\', '\,', '\;'), $lines[$key]);
 			$lines[$key] = preg_replace("/\r?\n/", '\n', $lines[$key]);
 		}
-		
+
 		return implode("\r\n ", $lines);
 	}
-	
+
 	function ical_time($time)
 	{
 		return $this->EE->localize->decode_date('%Y%m%dT%H%i%s', $time);
 	}
-	
+
 	function usage()
 	{
 		return <<<EOF
@@ -191,9 +191,9 @@ url="{url_title_path='group/template'}"
 update={number}
  This adds a SEQUENCE=(number) to the event. Needed if you update an entry, otherwise iCal won't update the event.
  Use a simple counter custom field, like http://github.com/GDmac/Reevision.ee_addon
-  
+
 --------------------------
- 
+
 EOF;
 	}
 }
