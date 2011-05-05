d22b73843098ce8abfec8a85c96e4160ac194c01
Adrian Macneil <adrian@crescendo.net.nz>
2011-05-05 12:48:59 +1200

    Removed content_type="debug", and renamed update="" to sequence="" to match iCalendar field name.

--- system/expressionengine/third_party/easy_ical/pi.easy_ical.php
+++ system/expressionengine/third_party/easy_ical/pi.easy_ical.php
@@ -31,15 +31,17 @@
 
 $plugin_info = array(
 	'pi_name'			=> 'Easy iCalendar',
-	'pi_version'		=> '1.1',
+	'pi_version'		=> Easy_ical::PI_VERSION,
 	'pi_author'			=> 'Crescendo Multimedia',
-	'pi_author_url'		=> 'http://www.crescendo.net.nz/',
+	'pi_author_url'		=> 'http://crescendo.net.nz/',
 	'pi_description'	=> 'Create valid iCalendars in seconds',
-	'pi_usage'			=> easy_ical::usage()
+	'pi_usage'			=> Easy_ical::usage()
 );
 
 class Easy_ical
 {
+	const PI_VERSION = '1.1';
+
 	function Easy_ical()
 	{
 		$this->EE =& get_instance();
@@ -59,7 +61,7 @@ class Easy_ical
 			$out .= "X-WR-CALNAME:".$this->escape($this->EE->TMPL->fetch_param('calname'))."\r\n";
 		}
 
-		$out .= "PRODID:-//EE Easy iCalendar plugin//NONSGML v1.1//EN\r\n";
+		$out .= 'PRODID:-//ExpressionEngine Easy iCalendar plugin//NONSGML v'.self::PI_VERSION .'//EN\r\n';
 
 		// EE has probably put heaps of useless whitespace between each entry
 		$tagdata = trim($this->EE->TMPL->tagdata);
@@ -70,14 +72,7 @@ class Easy_ical
 
 		// print output directly with the correct content-type
 		$content_type = $this->EE->TMPL->fetch_param('content_type');
-		if (empty($content_type)){
-			$content_type = 'text/calendar; charset=UTF-8';
-		}
-		else if($content_type=='debug')
-		{
-			$content_type = 'text/html; charset=UTF-8';
-			$out ="<pre>".$out;
-		}
+		if (empty($content_type)) $content_type = 'text/calendar; charset=UTF-8';
 
 		header('Content-Type: '.$content_type);
 		exit($out);
@@ -106,12 +101,9 @@ class Easy_ical
 			$out .= "SUMMARY:".$this->escape($this->EE->TMPL->fetch_param('summary'))."\r\n";
 		}
 
-		if ($this->EE->TMPL->fetch_param('update') !== FALSE)
+		if ($this->EE->TMPL->fetch_param('sequence') !== FALSE)
 		{
-			if($this->EE->TMPL->fetch_param('update') !=='')
-			{
-				$out .= "SEQUENCE:".$this->escape($this->EE->TMPL->fetch_param('update'))."\r\n";
-			}
+			$out .= "SEQUENCE:".$this->escape($this->EE->TMPL->fetch_param('sequence'))."\r\n";
 		}
 		if ($this->EE->TMPL->fetch_param('url') !== FALSE)
 		{
@@ -158,43 +150,11 @@ class Easy_ical
 		return $this->EE->localize->decode_date('%Y%m%dT%H%i%s', $time);
 	}
 
-	function usage()
+	public static function usage()
 	{
-		return <<<EOF
-Really basic, just construct a template with a channel entries tag, like so:
-
-{exp:easy_ical:calendar timezone="Pacific/Auckland" name="My Easy Event Calendar"}
-	{exp:channel:entries channel="events" show_future_entries="yes" show_expired="yes" limit="20"}
-		{exp:easy_ical:event uid="{entry_id}" start_time="{entry_date}" end_time="{expiration_date}" location="{event_location}" summary="{title}"}
-			{event_description}
-		{/exp:easy_ical:event}
-	{/exp:channel:entries}
-{/exp:easy_ical:calendar}
-
-All of the CRLF and escaping characters nonsense will be handled for you automatically.
-
-NOTE: Anything else in the template outside the {exp:easy_ical:calendar} tag will be ignored!
-
-Optional tag parameters
---------------------------
-content_type="debug"
- This will output the template with a html/text header and pre-tag for debugging. Or specify your own content_type.
- Example: {exp:easy_ical:calendar content_type="debug ... }
-
-
-Parameters for the event tag
-Example: {exp:easy_ical:event url="{url_title_path='group/template'}" update="{revision_num} ... }
-
-url="{url_title_path='group/template'}"
- This allows you to add a link to a calendar event entry
-
-update={number}
- This adds a SEQUENCE=(number) to the event. Needed if you update an entry, otherwise iCal won't update the event.
- Use a simple counter custom field, like http://github.com/GDmac/Reevision.ee_addon
-
---------------------------
-
-EOF;
+		// for performance only load README if inside control panel
+		$EE =& get_instance();
+		return isset($EE->cp) ? file_get_contents(PATH_THIRD.'easy_ical/README.md') : '';
 	}
 }
 
