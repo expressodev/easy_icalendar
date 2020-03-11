30a10f3ce0e6630a06be65df2e9a6662e0dae1cc
Matthew Kirkpatrick <mk@bidwrangler.com>
2020-03-11 12:34:14 -0400

    ExpressionEngine v3+ compatability

---
+++ easy_ical/config.php
@@ -0,0 +1,27 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Easy iCalendar (forked)
+ *
+ * @package     ExpressionEngine
+ * @category    Plugin
+ * @author      Matthew Kirkpatrick
+ * @copyright   Copyright (c) 2020, Matthew Kirkpatrick
+ * @link        https://github.com/javashakes
+ */
+
+if ( ! defined('EASY_ICAL_NAME'))
+{
+    define('EASY_ICAL_NAME',              'Easy iCalendar');
+    define('EASY_ICAL_VERSION',           '2.0.0');
+    define('EASY_ICAL_AUTHOR',            'Matthew Kirkpatrick');
+    define('EASY_ICAL_AUTHOR_URL',        'https://github.com/javashakes');
+    define('EASY_ICAL_DOCS',              'https://github.com/javashakes/easy_icalendar');
+    define('EASY_ICAL_DESC',              '<p><strong>Create valid iCalendar ICS files in seconds.</strong></p>Documentation and Usage: <a href="' . EASY_ICAL_DOCS . '" target="_blank">' . EASY_ICAL_DOCS . '</a>');
+    define('EASY_ICAL_PACKAGE',           'easy_ical');
+    define('EASY_ICAL_NAMESPACE',         'EasyiCal');
+    define('EASY_ICAL_CLASS_NAME',        'Easy_ical');
+}
+
+/* End of file config.php */
+/* Location: ./system/expressionengine/third_party/easy_ical/config.php */
--- easy_ical/pi.easy_ical.php
+++ easy_ical/pi.easy_ical.php
@@ -1,66 +1,70 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-/*
- * Easy iCalendar plugin by Crescendo (support@crescendo.net.nz)
+/**
+ * Easy iCalendar (forked)
  *
- * Copyright (c) 2010 Crescendo Multimedia Ltd
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
- * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
- * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * @package     ExpressionEngine
+ * @category    Plugin
+ * @author      Matthew Kirkpatrick
+ * @copyright   Copyright (c) 2020, Matthew Kirkpatrick
+ * @link        https://github.com/javashakes
  */
 
+// config
+include(PATH_THIRD.'easy_ical/config.php');
+
+// EE v2 backward compatibility
+// Ignored by EE v3+
 $plugin_info = array(
-    'pi_name'           => 'Easy iCalendar',
-    'pi_version'        => Easy_ical::VERSION,
-    'pi_author'         => 'Exp:resso',
-    'pi_author_url'     => 'http://exp-resso.com/',
-    'pi_description'    => 'Create valid iCalendars in seconds',
-    'pi_usage'          => Easy_ical::usage(),
+    'pi_name'           => EASY_ICAL_NAME,
+    'pi_version'        => EASY_ICAL_VERSION,
+    'pi_author'         => EASY_ICAL_AUTHOR,
+    'pi_author_url'     => EASY_ICAL_AUTHOR_URL,
+    'pi_description'    => EASY_ICAL_DESC
 );
 
 class Easy_ical
 {
-    const VERSION = '1.3';
 
+    public $return_data = '';
+
+    /**
+     * Constructor
+     *
+     * @access  public
+     * @return  string
+    */
+    public function __construct()
+    {
+        // default output
+        $this->return_data = '' . EASY_ICAL_NAME . '<br>' . EASY_ICAL_DESC;
+    }
+
+    /**
+     * CALENDAR CREATION
+     *
+     * @access  public
+     * @return  string
+    */
     public function calendar()
     {
-        $out = "BEGIN:VCALENDAR\r\nVERSION:2.0\r\n";
-        $out .= "PRODID:-//ExpressionEngine Easy iCalendar plugin//NONSGML v".self::VERSION ."//EN\r\n";
+        $this->return_data = "BEGIN:VCALENDAR\r\nVERSION:2.0\r\n";
+        $this->return_data .= "PRODID:-//ExpressionEngine Easy iCalendar plugin//NONSGML v". EASY_ICAL_VERSION ."//EN\r\n";
 
         if (ee()->TMPL->fetch_param('timezone') !== FALSE) {
-            $out .= "X-WR-TIMEZONE:".$this->escape(ee()->TMPL->fetch_param('timezone'))."\r\n";
+            $this->return_data .= "X-WR-TIMEZONE:".$this->escape(ee()->TMPL->fetch_param('timezone'))."\r\n";
         }
 
         if (ee()->TMPL->fetch_param('calendar_name') !== FALSE) {
-            $out .= "X-WR-CALNAME:".$this->escape(ee()->TMPL->fetch_param('calendar_name'))."\r\n";
+            $this->return_data .= "X-WR-CALNAME:".$this->escape(ee()->TMPL->fetch_param('calendar_name'))."\r\n";
         }
 
-        // EE has probably put heaps of useless whitespace between each entry
+        // trim away whitespace between each entry
         $tagdata = trim(ee()->TMPL->tagdata);
         $tagdata = preg_replace('/END\:VEVENT\s*BEGIN\:VEVENT/', "END:VEVENT\r\nBEGIN:VEVENT", $tagdata);
-        if (!empty($tagdata)) $out .= $tagdata."\r\n";
+        if (!empty($tagdata)) $this->return_data .= $tagdata."\r\n";
 
-        $out .= "END:VCALENDAR";
+        $this->return_data .= "END:VCALENDAR";
 
         // print output directly with the correct content-type
         $content_type   = ee()->TMPL->fetch_param('content_type');
@@ -75,48 +79,60 @@ class Easy_ical
         header('Cache-Control: must-revalidate');
         header('Pragma: public');
         // header('Content-Length: ' . filesize($file));
-        exit($out);
+        exit($this->return_data);
     }
 
+    /**
+     * CALENDAR EVENT CREATION
+     *
+     * @access  public
+     * @return  string
+    */
     public function event()
     {
-        $out = "BEGIN:VEVENT\r\n".
+        $this->return_data = "BEGIN:VEVENT\r\n".
             "UID:".$this->escape(ee()->TMPL->fetch_param('uid'))."\r\n";
 
         if (ee()->TMPL->fetch_param('location') !== FALSE) {
-            $out .= "LOCATION:".$this->escape(ee()->TMPL->fetch_param('location'))."\r\n";
+            $this->return_data .= "LOCATION:".$this->escape(ee()->TMPL->fetch_param('location'))."\r\n";
         }
 
-        $out .= "DTSTAMP:".$this->ical_time(ee()->TMPL->fetch_param('start_time'))."\r\n";
-        $out .= "DTSTART:".$this->ical_time(ee()->TMPL->fetch_param('start_time'))."\r\n";
+        $this->return_data .= "DTSTAMP:".$this->ical_time(ee()->TMPL->fetch_param('start_time'))."\r\n";
+        $this->return_data .= "DTSTART:".$this->ical_time(ee()->TMPL->fetch_param('start_time'))."\r\n";
 
         if (ee()->TMPL->fetch_param('end_time') != FALSE) {
-            $out .= "DTEND:".$this->ical_time(ee()->TMPL->fetch_param('end_time'))."\r\n";
+            $this->return_data .= "DTEND:".$this->ical_time(ee()->TMPL->fetch_param('end_time'))."\r\n";
         } else {
-            $out .= "DTEND:".$this->ical_time(strtotime(ee()->TMPL->fetch_param('start_time')) + 3600)."\r\n";
+            $this->return_data .= "DTEND:".$this->ical_time(strtotime(ee()->TMPL->fetch_param('start_time')) + 3600)."\r\n";
         }
 
         if (ee()->TMPL->fetch_param('summary') !== FALSE) {
-            $out .= "SUMMARY:".$this->escape(ee()->TMPL->fetch_param('summary'))."\r\n";
+            $this->return_data .= "SUMMARY:".$this->escape(ee()->TMPL->fetch_param('summary'))."\r\n";
         }
 
         if (ee()->TMPL->fetch_param('sequence') !== FALSE) {
-            $out .= "SEQUENCE:".$this->escape(ee()->TMPL->fetch_param('sequence'))."\r\n";
+            $this->return_data .= "SEQUENCE:".$this->escape(ee()->TMPL->fetch_param('sequence'))."\r\n";
         }
         if (ee()->TMPL->fetch_param('url') !== FALSE) {
-            $out .= "URL:".$this->escape(ee()->TMPL->fetch_param('url'))."\r\n";
+            $this->return_data .= "URL:".$this->escape(ee()->TMPL->fetch_param('url'))."\r\n";
         }
 
         $description = trim(ee()->TMPL->tagdata);
         if (!empty($description)) {
-            $out .= "DESCRIPTION:".$this->escape($description)."\r\n";
+            $this->return_data .= "DESCRIPTION:".$this->escape($description)."\r\n";
         }
 
-        $out .= "END:VEVENT"."\r\n";
+        $this->return_data .= "END:VEVENT"."\r\n";
 
-        return $out;
+        return $this->return_data;
     }
 
+    /**
+     * HELPER: STRING ESCAPING/CLEANING
+     *
+     * @access  public
+     * @return  string
+    */
     public function escape($str)
     {
         // strip any html tags
@@ -140,14 +156,18 @@ class Easy_ical
         return implode("\r\n ", $lines);
     }
 
+    /**
+     * HELPER: DATE FORMATTING
+     *
+     * @access  public
+     * @return  string
+    */
     public function ical_time($time)
     {
         return ee()->localize->format_date('%Y%m%dT%H%i%s', $time);
     }
 
-    public static function usage()
-    {
-        // for performance only load README if inside control panel
-        return REQ == 'CP' ? file_get_contents(PATH_THIRD.'easy_ical/README.md') : '';
-    }
 }
+
+/* End of file pi.easy_ical.php */
+/* Location: /system/expressionengine/third_party/easy_ical/pi.easy_ical.php */
